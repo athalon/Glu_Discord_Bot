72e2e8924cb50f667a38549c4bb25ff2ab2e06a5
athalon <58222464+athalon@users.noreply.github.com>
2021-03-04 17:29:11 +0100

    Mechmed

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_prefix(client, message):
 client = commands.Bot(command_prefix = get_prefix)
 
 client.remove_command('help')  #huhrensohn
-
+# Selber Hurensohn
 @client.event
 async def on_ready():
   print("Bot is Ready")
